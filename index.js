57c6d1a4f65aef8db7bf0f1ca975a4603b464124
Subramanian <Subramanian.Palanikumar@td.com>
2017-01-14 14:12:21 -0500

    base

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+
+app.get('/', function(request, response) {
+  var output = {
+		name : "Subbu",
+		firstName : "Subramanian",
+		lastName : "Palanikumar"
+	};
+
+	response.json(output);
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
